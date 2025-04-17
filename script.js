c439cb0b0ef1f5120be1e67fbcd98d2c4e5915fb
ageuxo <ageuxo@gmail.com>
2025-04-17 12:20:18 +0200

    minor edits

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const canvas = document.getElementById("canvas");
 const strokeWidthSlider = document.getElementById("stroke-width");
 const fillToggle = document.getElementById("fill-toggle");
 
-let webSocket = new WebSocket("ws://localhost:55455");
+let webSocket = new WebSocket("wss://ageuxo.org:55455");
+console.log("Websocket created on " + webSocket.url);
 
 const sendNewEntity = (entity)=> {
   const payload = {
